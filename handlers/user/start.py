9a14f060c7342e0f4cb574413eace4d5f8100655
Bulat Z <kusok.apelbsina@gmail.com>
2022-09-10 12:02:41 +0300

    feature: added inline keyboard for adding balance

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -5,9 +5,12 @@ from aiogram.dispatcher.filters import CommandStart
 
 from data import messages
 
+from keyboards.inline import add_balance_key
+
 
 @dp.message_handler(CommandStart())
 async def send_start(message: types.Message):
     # TODO here keyboard
     text = messages.START_MESSAGE_TEMPLATE.format(username=message.from_user.first_name)
-    await message.answer(text)
+    keyboard = add_balance_key.get()
+    await message.answer(text, reply_markup=keyboard)
--- keyboards/inline/add_balance_key.py
+++ keyboards/inline/add_balance_key.py
@@ -1,1 +1,13 @@
-from aiogram
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.callback_data import CallbackData
+
+
+balance_cb = CallbackData('balance', 'action')
+
+
+def get():
+    markup = InlineKeyboardMarkup()
+    markup.add(
+        InlineKeyboardButton('Пополнить баланс', callback_data=balance_cb.new('add'))
+    )
+    return markup 
